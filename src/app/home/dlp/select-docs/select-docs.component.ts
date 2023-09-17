c9936118c8277572586720bf4a54870f0abf480d
Vivek <helpingworlgivesmeecstacy@gmail.com>
2023-09-18 00:28:31 +0530

    feat : added featue to select the docs

---
+++ src/app/home/dlp/select-docs/select-docs.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataBaseService } from 'src/app/service/data-base.service';
+import { HomeService } from '../../home.service';
+
+interface file{
+  index: number;
+  name : string;
+  uri : string;
+}
+
+@Component({
+  selector: 'app-select-docs',
+  templateUrl: './select-docs.component.html',
+  styleUrls: ['./select-docs.component.css']
+})
+
+export class SelectDocsComponent implements OnInit{
+  @Output() commandEvent = new EventEmitter<number[]>();
+  
+   fileIndexUri : file[] = [];
+   selectedFiles : number[] = [];
+   constructor(private _dataBaseService : DataBaseService){}
+   
+
+   async ngOnInit() { 
+      this.fileIndexUri = await this._dataBaseService.getIndexAndUri();
+   }
+
+   toggleSelection(index: number) {
+    // Toggle the selection of the checkbox index
+    if (this.selectedFiles.includes(index)) {
+      this.selectedFiles = this.selectedFiles.filter(item => item !== index);
+    } else {
+      this.selectedFiles.push(index);
+    }
+  }
+
+  generateNewDlp() {
+    // This method is called when the "Show Selected Options" button is clicked
+    console.log('Selected Options:', this.selectedFiles);
+    this.commandEvent.emit(this.selectedFiles);
+    // You can perform further actions with the selected options here
+  }
+}
